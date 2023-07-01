e510d9c004a9363f2ba8282f783fdb3509fc18eb
Aiman Aeffil <aimanaeffil1012@gmail.com>
2023-07-01 20:51:16 +0800

    Intial commit

---
+++ src/main/java/com/aeffil/imagegallerydemo/ImageGalleryDemoApplication.java
@@ -0,0 +1,13 @@
+package com.aeffil.imagegallerydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageGalleryDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImageGalleryDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aeffil/imagegallerydemo/ImageGalleryDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aeffil.imagegallerydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageGalleryDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
